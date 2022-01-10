dd19f66ca51e10218665cb18af8acbfcf30b4f4c
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-01-10 12:31:11 +0300

    qwe

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import './sass/main.scss'
+import React from 'react'
 
-function App() {
+const App = () => {
     return (
-        <>
-
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-
-                    <div className="container">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptatibus eum fuga eligendi. Ratione totam dignissimos reprehenderit quidem quisquam iusto voluptatum enim qui? Eligendi nobis, non corrupti molestiae suscipit provident.
-                    </div>
-                </header>
-            </div>
-
+        <div>
             <div className="container">
                 <div className="row">
-                    <h1>lorem</h1>
+                    <div className="oc">qw21q231</div>
+
                 </div>
             </div>
-        </>
-    );
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './sass/main.scss'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Switch>
+      <Route path='/' exact component={App} />
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
