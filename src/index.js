e7add3f76c1ca0a149978497cb65bb408f0f9f77
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-02-07 18:11:41 +0500

    koko

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./sass/main.scss";
@@ -22,6 +22,8 @@ import Phone from "./components/Phone";
 import Blog from "./pages/Blog";
 import MyModal from "./components/MyModal";
 
+import { hydrate, render } from "react-dom";
+
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -32,47 +34,59 @@ import { Provider } from "react-redux";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
-ReactDOM.render(
-  <>
-    <Provider store={store}>
-      <Router>
-        <Navbar />
-        <ScrollToTop />
-        <Switch>
-          <Route path="/" exact component={App} />
-          <Route path="/blog" exact component={Blog} />
-          <Route path="/about-us" exact component={AboutUs} />
-          <Route path="/projects" exact component={AllPortfolio} />
-          <Route path="/contacts" exact component={ContactsPage} />
-          <Route path="/web-development" exact component={WebDevelopment} />
-          <Route
-            path="/internet-marketing"
-            exact
-            component={InternetMarketing}
-          />
-          <Route path="/design" exact component={Design} />
-          <Route path="/motion" exact component={Moushen} />
-          {/* <Route path='/services' exact component={Services} /> */}
-        </Switch>
 
-        <Phone />
-        <Footer2 />
-        <MyModal />
-        <ToastContainer
-          position="top-left"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          theme='dark'
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          style={{zIndex: '999999999'}}
+const APP = (<>
+  <Provider store={store}>
+    <Router>
+      <Navbar />
+      <ScrollToTop />
+      <Switch>
+        <Route path="/" exact component={App} />
+        <Route path="/blog" exact component={Blog} />
+        <Route path="/about-us" exact component={AboutUs} />
+        <Route path="/projects" exact component={AllPortfolio} />
+        <Route path="/contacts" exact component={ContactsPage} />
+        <Route path="/web-development" exact component={WebDevelopment} />
+        <Route
+          path="/internet-marketing"
+          exact
+          component={InternetMarketing}
         />
-      </Router>
-    </Provider>
-  </>,
-  document.getElementById("KOKO")
-);
+        <Route path="/design" exact component={Design} />
+        <Route path="/motion" exact component={Moushen} />
+        {/* <Route path='/services' exact component={Services} /> */}
+      </Switch>
+
+      <Phone />
+      <Footer2 />
+      <MyModal />
+      <ToastContainer
+        position="top-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        theme='dark'
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        style={{zIndex: '999999999'}}
+      />
+    </Router>
+  </Provider>
+</>)
+ 
+const rootElement = document.getElementById("KOKO");
+if (rootElement.hasChildNodes()) {
+  hydrate(APP, rootElement);
+} else {
+  render(APP, rootElement);
+}
+
+
+
+// ReactDOM.render(
+//   ,
+//   document.getElementById("KOKO")
+// );
