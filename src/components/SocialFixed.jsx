fafbfcf655344f0001449c6703d9e018fb89880b
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-02-15 15:16:14 +0500

    koko

--- src/components/SocialFixed.jsx
+++ src/components/SocialFixed.jsx
@@ -4,9 +4,9 @@ const SocialFixed = () => {
     return (
         <>
             <div className="socialFixed">
-                <a target='_blank' rel="noreferrer" href="https://www.instagram.com/kokoagency.uz/" className=' cursor'><img src="/assets/icon/ins.svg" alt="" /></a>
-                <a target='_blank' rel="noreferrer" href="https://t.me/kOkOdigital/" className='cursor'><img src="/assets/icon/tg.svg" alt="" /></a>
-                <a target='_blank' rel="noreferrer" href="https://www.facebook.com/kokoagency.uz/?ref=pages_you_manage" className='cursor'><img src="/assets/image/facebook.png" alt="" /></a>
+                <a target='_blank' rel="noreferrer" href="https://www.instagram.com/kokoagency.uz/" className=' cursor'><i className={`icon icon-instagram ${localStorage.getItem('theme') === 'white' ? 'black' : ''}`}></i></a>
+                <a target='_blank' rel="noreferrer" href="https://t.me/kOkOdigital/" className='cursor'><i className={`icon icon-telegram ${localStorage.getItem('theme') === 'white' ? 'black' : ''}`}></i></a>
+                <a target='_blank' rel="noreferrer" href="https://www.facebook.com/kokoagency.uz/?ref=pages_you_manage" className='cursor'><i className={`icon icon-facebook ${localStorage.getItem('theme') === 'white' ? 'black' : ''}`}></i></a>
 
                 <h6>Follow us</h6>
             </div>
--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -6,7 +6,7 @@ const Support = () => {
             <div className="container">
                 <div className="row align-items-center text-center text-lg-left justify-content-center">
                     <div data-aos='fade-right' className="col-lg-2 mb-lg-0 mb-4">
-                        <i><img src="/assets/image/contactBg.svg" alt="" /></i>
+                        <i className={`icon icon-support ${localStorage.getItem('theme') === 'white' ? 'black' : ''}`}></i>
                     </div>
                     <div data-aos='fade-up' className="col-lg-4 text-center">
                         <h1>Звоните прямо сейчас </h1>
@@ -14,7 +14,7 @@ const Support = () => {
                         <a className='btn myBtn' href="tel: +998908232838">Позвонить</a>
                     </div>
                     <div data-aos='fade-left' className="col-lg-2 mb-lg-0 d-none d-lg-flex mb-4">
-                        <i><img src="/assets/image/contactBg.svg" alt="" /></i>
+                        <i className={`icon icon-support ${localStorage.getItem('theme') === 'white' ? 'black' : ''}`}></i>
                     </div>
                 </div>
             </div>
