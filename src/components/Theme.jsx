4321c57c7be63b137ed11dbcbab90a4bcdec0019
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-02-15 11:46:42 +0500

    koko

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -3,20 +3,30 @@ import { connect } from 'react-redux'
 import { updateState } from '../redux/actions/emailAction'
 
 const Theme = (props) => {
+    const blackHandler = () => {
+        document.location.reload(true);
+        localStorage.setItem('theme', 'black')
+    }
+    const whiteHandler = () => {
+        document.location.reload(true);
+        localStorage.setItem('theme', 'white')
+    }
     return (
         <>
 
             <div className="fixedTheme">
                 <h6
                     className={` ${props.white ? '' : 'active'}`}
-                    onClick={() => localStorage.setItem('theme', 'black')}
-                    // onClick={() => props.updateState({white: false})}
-                    >
+                    onClick={blackHandler}
+                    // onClick={() => localStorage.setItem('theme', 'black')}
+                // onClick={() => props.updateState({white: false})}
+                >
                     Dark</h6>
-                <h6 
+                <h6
                     className={`${props.white ? 'actives' : ''}`}
-                    onClick={() => localStorage.setItem('theme', 'white')}
-                    // onClick={() => props.updateState({white: true})}
+                    onClick={whiteHandler}
+                    // onClick={() => localStorage.setItem('theme', 'white')}
+                // onClick={() => props.updateState({white: true})}
                 >Light</h6>
             </div>
 
@@ -30,4 +40,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps, {updateState})(Theme)        
+export default connect(mapStateToProps, { updateState })(Theme)        
