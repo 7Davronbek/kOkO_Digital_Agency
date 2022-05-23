d129f49dfdcfc07797b0f65dd6798a445747a395
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-05-23 17:59:56 +0500

    kOkO

---
+++ src/components/MobileHeader.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react'
+import Aos from 'aos';
+
+const MobileHeader = () => {
+
+    useEffect(() => {
+        Aos.init({
+            once: false,
+            duration: 1700,
+        })
+    })
+
+    return (
+        <>
+
+            <div className='mobileHeader myWebHeader'>
+                <div className="container">
+                    <div className="row align-items-center flex-wrap-reverse">
+                        <div data-aos='fade-right' className="col-lg-6">
+                            <h1>Разработка приложенний</h1>
+                            <p>Разработка сайтов – это создания веб-сайта где основными этапами этого процесса являются веб-дизайн, верстка страниц и  конфигурирование.</p>
+                            <p>С помощью сайта вы с лёгкостью сможете показать свой продукт или компанию пользователям интернета. При заказе веб услуг в нашем агентстве, вы станете свидетелями качественной и быстрой работы нашей команды. Мы уверенны, что результаты будут вам по душе.</p>
+                        </div>
+                        <div data-aos='fade-left' className="col-lg-6">
+                            <i><img style={{ borderRadius: '20px' }} className='w-100' src="/assets/image/app/1.webp " alt="" /></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default MobileHeader
+
--- src/pages/MobileDevelopment.jsx
+++ src/pages/MobileDevelopment.jsx
@@ -1,13 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Helmet from 'react-helmet'
+import { RingLoader } from 'react-spinners'
+import MobileHeader from '../components/MobileHeader'
 
 const MobileDevelopment = () => {
-  return (
-    <>
-    
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam explicabo eius ab rerum debitis possimus excepturi, ratione at odit ducimus tempora reiciendis, laudantium, animi architecto rem similique dolorem fuga atque?
-    
-    </>
-  )
+    const [isLoading, setIsLoading] = useState(true);
+    const [load, setLoad] = useState(false);
+  
+    useEffect(() => {
+      setTimeout(() => {
+        setIsLoading(false);
+        setLoad(true);
+      }, 1800);
+    }, []);
+    return (
+        <>
+
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>kOkO Agency | Web Development | Tashkent | KOKO | koko</title>
+                <link rel="canonical" href="https://www.kokoagency.uz/" />
+            </Helmet>
+            {isLoading ? (
+                <div className="loader ">
+                    <h5 className='mr-4'>Разработка приложенний</h5>
+                    <RingLoader color={`${localStorage.getItem('theme') === 'white' ? '#000' : '#fff'}`} size="35" loading={isLoading} />
+                </div>
+            ) : (
+                ""
+            )}
+            <div className='mobileDevelopment courses'>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 text-center">
+                            {load ? (
+                                <>
+                                    <h1 data-aos='fade-right'>Mobile developing</h1>
+
+                                    <a
+                                        href="https://t.me/kOkOdigital"
+                                        target='_blank' rel="noreferrer"
+                                        className="btn myBtn"
+                                        data-aos='fade-left'
+                                    // onClick={() => props.updateState({ isOpen: true })}
+                                    // onClick={() => setModal(true)}
+                                    >
+                                        <div className="click"></div>
+                                        Бесплатная консультация
+                                    </a>
+                                </>
+                            ) : null}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <MobileHeader />
+        </>
+    )
 }
 
 export default MobileDevelopment
